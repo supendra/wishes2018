bfaf172b3d9cdc7f49ba1aeea09357e3325a6e72
unknown <supendradev@gmail.com>
2017-11-20 23:00:46 +0530

    commit

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+
+    $dbhost = 'wishes2018.tk';
+    $dbuser = 'iunlocki_surya';
+    $dbpass = 'suriya2.';
+    $dbname = 'iunlocki_firstwishes';
+
+    $con=mysqli_connect($dbhost, $dbuser, $dbpass,$dbname) //connect to the database server
+    or die ("Could not connect to mysql because ".mysqli_error());
+    mysqli_query($con, "SET NAMES 'utf8'");
+    mysqli_select_db($con,$dbname)  //select the database
+    or die ("Could not select to mysql because ".mysqli_error());
+
+    ?>
